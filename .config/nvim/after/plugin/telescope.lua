07ec2d6093c44844c9fee58ffbf0b5c2b49b3859
EldevM <czechu600@gmail.com>
2024-06-03 01:07:58 +0200

    updated telescope binds

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,17 +1,32 @@
 local builtin = require('telescope.builtin')
 
+local ignore_patterns = { '.git', 'node_modules' };
+
 vim.keymap.set('n', '<leader>pd', function()
+
     require('telescope.builtin').find_files({
-        hidden = true
+        hidden = true,
+        find_command = { 'rg', '--files', '--no-ignore' },
     })
+
 end, { silent = true })
 
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pa', builtin.find_files, {})
 
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>pa', function()
+
+    require('telescope.builtin').find_files({
+        hidden = true,
+        file_ignore_patterns = ignore_patterns
+    })
+
+end, { silent = true })
+
+vim.keymap.set('n', '<leader>pS', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 
