ffff4454b4d877a0ab3a977d0d726ad1680cd5e5
EldevM <czechu600@gmail.com>
2024-12-06 21:47:27 +0100

    no auto attach in remote shell

--- .zsh/binds/auto-attach-session.zsh
+++ .zsh/binds/auto-attach-session.zsh
@@ -1,6 +1,6 @@
 #
 # run ta command when shell is interactive, AND not running in tmux pane
-if [[ -o interactive && -z $TMUX ]]; then
+if [[ -o interactive && -z $TMUX && -z "$SSH_CONNECTION" ]]; then
     if tty | grep -q 'pts' ; then
         ta
     fi
