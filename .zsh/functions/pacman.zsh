d5adcb290925f38e7a9a8bd1b205f8cda4efc737
Storm-77 <czechu600@gmail.com>
2025-03-21 16:50:57 +0100

    refactored pacman shell wrappers

--- .zsh/functions/pacman.zsh
+++ .zsh/functions/pacman.zsh
@@ -1,20 +1,54 @@
 pacrem(){
-    # delete selected package and its dependencies that are not required by other packages
-    pacman -Qe | fzf | awk '{print $1}' | xargs -o -r sudo pacman -Rs
+
+    explicitly_installed_mode(){
+        # delete selected package and its dependencies that are not required by other packages
+          pacman -Qe | fzf -m | awk '{print $1}' | xargs -o -r sudo pacman -Rs
+    }
+          
+    # Check if any arguments are provided
+    if [ $# -eq 0 ]; then
+        explicitly_installed_mode
+    fi 
+
+
+    while getopts ":har" opt; do
+      case $opt in
+        h)
+            echo "Pacrem, mini tui for pacman\n"
+            echo "\tAvailable flags: (only first takes effect)"
+            echo "\t-h\tdisplay help and exit"
+            echo "\t-r\tuse recursive mode, uninstall package dependencies and all packages that depend on this package"
+            echo "\t-a\tall packaes mode, lists packages installed as dependencies as well as explicitly installed packages"
+            echo "\n\tWhen no arguments provided default mode lists explicitly installed packages"
+            break
+          ;;
+        a)
+            # delete selected packages and their dependencies that are not required by other packages
+            pacman -Q | fzf -m | awk '{print $1}' | xargs -o -r sudo pacman -Rs
+            break
+          ;;
+        r)
+            # delete selected package and its dependencies, and everything that depends on that package
+            # use with caution
+            pacman -Q | fzf | awk '{print $1}' | xargs -o -r sudo pacman -Rcs
+            break
+          ;;
+        \?)
+            echo "Unknown flag see: [pacrem -h]"
+            break
+            ;;
+        :)
+          echo "Default mode"
+          explicitly_installed_mode
+          break
+          ;;
+      esac
+    done
+
+
 }
 
-pacrema(){
-    # delete selected package and its dependencies that are not required by other packages
-    pacman -Q | fzf | awk '{print $1}' | xargs -o -r sudo pacman -Rs
-}
-
-pacrema-recurs(){
-    # delete selected package and its dependencies, and everything that depends on that package
-    # use with caution
-    pacman -Q | fzf | awk '{print $1}' | xargs -o -r sudo pacman -Rcs
-}
-
-pacfindf(){
+pacfind(){
     whence -pm '*' | fzf | xargs -o -r pacman -Qo
 }
 
--- .zsh/functions/temporary-ssh.zsh
+++ .zsh/functions/temporary-ssh.zsh
@@ -1,6 +1,4 @@
-
 tssh() {
-    #do things with parameters like $1 such as
-    ssh $1 -o "UserKnownHostsFile=/dev/null"
+    ssh $@ -o "UserKnownHostsFile=/dev/null"
 }
 
